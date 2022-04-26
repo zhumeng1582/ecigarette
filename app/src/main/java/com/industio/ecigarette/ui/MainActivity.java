b9dca4477892923e49766189194e9ca2d686a3f1
zhangchao <zhumeng1582@163.com>
2022-04-26 17:36:00 +0800

    串口

--- app/src/main/java/com/industio/ecigarette/ui/MainActivity.java
+++ app/src/main/java/com/industio/ecigarette/ui/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.blankj.utilcode.util.ClickUtils;
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.ThreadUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.industio.ecigarette.R;
 import com.industio.ecigarette.databinding.ActivityMainBinding;
+
+import org.ido.iface.SerialControl;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ActivityMainBinding binding;
@@ -19,15 +26,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         ClickUtils.applySingleDebouncing(new View[]{
+                binding.btnPlayMusic,
                 binding.btnMode,
+                binding.btnSetPara,
         }, this);
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view == binding.btnMode) {
+        if (view == binding.btnPlayMusic) {
+        } else if (view == binding.btnMode) {
             startActivity(new Intent(MainActivity.this, ParaActivity.class));
+        } else if (view == binding.btnSetPara) {
         }
+    }
+
+    //初始化串口
+    private void initSerial() {
+
+        SerialControl mErrLogSerialControl = new SerialControl() {
+            @Override
+            protected void read(byte[] buf, int len) {
+
+            }
+        };
+
+        String portName = "";
+        if (mErrLogSerialControl.init(portName, 9600, 8, 'N', 1, 0, 10)) {
+            ToastUtils.showShort("打开成功:" + portName);
+        } else {
+            ToastUtils.showLong("打开失败:" + portName);
+        }
+
     }
 }
