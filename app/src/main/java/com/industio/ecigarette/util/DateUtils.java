55d075bdea61413249c04672294360217da0bde4
zhangchao <zhumeng1582@163.com>
2022-06-13 10:17:54 +0800

    图表显示优化

--- app/src/main/java/com/industio/ecigarette/util/DateUtils.java
+++ app/src/main/java/com/industio/ecigarette/util/DateUtils.java
@@ -1,14 +1,10 @@
 package com.industio.ecigarette.util;
 
-import android.util.Pair;
-
-import com.blankj.utilcode.util.StringUtils;
 import com.blankj.utilcode.util.TimeUtils;
 import com.industio.ecigarette.bean.CigaretteData;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DateUtils {
     /**
@@ -18,23 +14,19 @@ public class DateUtils {
     private static SimpleDateFormat sdfy = new SimpleDateFormat("yyyy");
     private static SimpleDateFormat sdfyM = new SimpleDateFormat("yyyy-MM");
     private static SimpleDateFormat sdfyH = new SimpleDateFormat("HH");
-    private static SimpleDateFormat sdfyDD = new SimpleDateFormat("dd");
+    public static SimpleDateFormat sdfyDD = new SimpleDateFormat("M月d日");
 
-    public static List<Pair<Integer, Integer>> getDataDay(ArrayList<CigaretteData> list) {
+    public static int[] getDataDay(ArrayList<CigaretteData> list) {
         int[] dataDay = new int[12];
         for (CigaretteData cigaretteData : list) {
             if (TimeUtils.isToday(cigaretteData.getTime())) {
                 String hourStr = TimeUtils.millis2String(cigaretteData.getTime(), sdfyH);
-                int hour = Integer.parseInt(hourStr);
+                int hour = Integer.parseInt(hourStr)/2;
                 dataDay[hour] = dataDay[hour] + 1;
             }
         }
-        List<Pair<Integer, Integer>> dataDayList = new ArrayList<>();
-        for (int i = 0; i < dataDay.length; i++) {
-            dataDayList.add(new Pair<>(i * 2, dataDay[i]));
-        }
 
-        return dataDayList;
+        return dataDay;
 //        return new int[]{1,2,3,4,5,6,7,7,8,9,0,1};
     }
 
@@ -60,7 +52,7 @@ public class DateUtils {
         return dataDay;
     }
 
-    public static List<Pair<Integer, Integer>> getDataWeek(ArrayList<CigaretteData> list) {
+    public static int[] getDataWeek(ArrayList<CigaretteData> list) {
         int[] dataDay = new int[7];
         for (CigaretteData cigaretteData : list) {
             for (int i = 0; i < 6; i++) {
@@ -69,15 +61,8 @@ public class DateUtils {
                 }
             }
         }
-        List<Pair<Integer, Integer>> dataDayList = new ArrayList<>();
-        for (int i = 0; i < dataDay.length; i++) {
-            long time = TimeUtils.getNowMills() - (6 - i) * 86400000;
-            String DD = TimeUtils.millis2String(time, sdfyDD);
 
-            dataDayList.add(new Pair<>(Integer.valueOf(DD), dataDay[i]));
-        }
-
-        return dataDayList;
+        return dataDay;
 //        return new int[]{1,2,3,4,5,6,7};
     }
 }
